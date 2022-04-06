0843e690c3e714576bcc80f3178d7ac4d11646d2
Marius <marius.badrajan@mail.ru>
2022-04-06 23:20:49 +0300

    Add path generation

---
+++ LSystemParser/LSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Text;
+using Antlr4.Runtime;
+
+namespace LSystem;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var path = "test.dsl";
+
+        var fileContent = File.ReadAllText(path);
+
+        var inputStream = new AntlrInputStream(fileContent);
+        var lexer = new DslLexer(inputStream);
+        var commonTokenStream = new CommonTokenStream(lexer);
+        var parser = new DslParser(commonTokenStream);
+        var context = parser.program();
+        var visitor = new DslVisitor();        
+        visitor.Visit(context);
+        ;
+    }
+}
